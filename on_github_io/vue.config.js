3dbf7c3ac71ba2a920b4f27048d27ac9358e2c4a
cjstegeman <41910090+cjstegeman@users.noreply.github.com>
2023-02-14 17:31:20 +0100

    Delete on_github_io directory

--- on_github_io/vue.config.js
+++
@@ -1,7 +0,0 @@
-/* const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-}) */
-module.exports = {
-    publicPath: '/on_github_io/'
-}
